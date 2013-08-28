9702478f1b93b6238ad8717ac35a7dee3c6e8e0d
OpenShift guest <wchargin@gmail.com>
2013-08-28 12:31:30 -0400

    Search algorithm interface

---
+++ SearchSort/src/apcs/searchsort/search/SearchAlgorithm.java
@@ -0,0 +1,24 @@
+package apcs.searchsort.search;
+
+import java.util.List;
+
+/**
+ * A searching algorithm.
+ * 
+ * @author William Chargin
+ * 
+ */
+public interface SearchAlgorithm {
+
+	/**
+	 * Finds the given element in the given list and returns the index at which
+	 * it occurs.
+	 * 
+	 * @param list
+	 *            the list to search
+	 * @param target the item to search for
+	 * @return the index of the item in the list, or {@code -1} if not found
+	 */
+	public <T> int indexOf(List<T> list, T target);
+
+}
