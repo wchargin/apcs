229030e294ab5776732e9a7dde66472243f3e69b
OpenShift guest <wchargin@gmail.com>
2013-08-28 12:21:35 -0400

    Implemented insertion sort

---
+++ SearchSort/src/apcs/searchsort/sort/InsertionSort.java
@@ -0,0 +1,28 @@
+package apcs.searchsort.sort;
+
+import java.util.List;
+
+/**
+ * Basic insertion sort. Runs in O(n<sup>2</sup>) time.
+ * 
+ * @author William Chargin
+ * 
+ */
+public class InsertionSort implements SortingAlgorithm {
+
+	@Override
+	public <T extends Comparable<? super T>> List<T> sort(List<T> list) {
+		for (int i = 1; i < list.size(); i++) {
+			int indexToInsert = 0;
+			T item = list.get(i);
+			while (indexToInsert < i &&
+			       list.get(indexToInsert).compareTo(item) < 0) {
+			    indexToInsert++;
+			}
+			list.remove(i);
+			list.add(indexToInsert, item);
+		}
+		return list;
+	}
+
+}
