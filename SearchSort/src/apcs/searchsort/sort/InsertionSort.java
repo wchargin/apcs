f3d5f6e11c92ce6b81d188928b1ed73b2f9d7db2
wchargin <wchargin@gmail.com>
2013-08-28 08:46:39 -0700

    Created insertion sort algorithm

---
+++ SearchSort/src/apcs/searchsort/sort/InsertionSort.java
@@ -0,0 +1,41 @@
+package apcs.searchsort.sort;
+
+import java.util.List;
+
+/**
+ * Basic insertion sort. Runs in O(n<sup>2</sup>) time.
+ * 
+ * @author William Chargin
+ * 
+ */
+public class InsertionSort implements SortingAlgorithm {
+
+	@Override
+	public <T extends Comparable<? super T>> List<T> sort(List<T> list) {
+		for (int i = 0; i < list.size(); i++) {
+			// minimum index of list[i:] defaults to i
+			int minimum = i;
+			T minValue = list.get(i);
+
+			for (int j = i; j < list.size(); j++) {
+				T jValue = list.get(j);
+				int compared = minValue.compareTo(jValue);
+				// compared <=> 0 as minValue <=> jValue
+
+				if (compared > 0) {
+					// "minimum" greater than j
+					minValue = jValue;
+					minimum = j;
+				}
+			}
+
+			// do a swap
+			if (minimum != i) {
+				list.set(minimum, list.get(i));
+				list.set(i, minValue);
+			}
+		}
+		return list;
+	}
+
+}
