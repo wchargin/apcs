6e02f008f589fe5647b525037c0050f261e69323
William Chargin <wchargin@gmail.com>
2013-08-31 23:01:17 -0700

    Formatted code (Ctrl Shift F)

--- SearchSort/src/apcs/searchsort/search/LinearSearch.java
+++ SearchSort/src/apcs/searchsort/search/LinearSearch.java
@@ -6,17 +6,16 @@ import java.util.List;
  * A basic linear search implementation. Runs in O(<sup>n</sup>) time.
  */
 public class LinearSearch implements SearchAlgorithm {
-    
-    @Override
-    public <T extends Comparable<? super T>> int indexOf(List<T> list, T target) {
-        for (int i=0; i<list.size(); i++) {
-            T item = list.get(i);
-            if ((target == null && item == null)
-             || (target.equals(item))) {
-                return i;
-            }
-        }
-        return -1;
-    }
-    
+
+	@Override
+	public <T extends Comparable<? super T>> int indexOf(List<T> list, T target) {
+		for (int i = 0; i < list.size(); i++) {
+			T item = list.get(i);
+			if ((target == null && item == null) || (target.equals(item))) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
 }
