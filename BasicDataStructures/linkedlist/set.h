48092d762205b856de00dd5a2604bb5c38678015
William Chargin <wchargin@gmail.com>
2013-09-05 00:37:16 -0400

    Created set interface (header file)

---
+++ BasicDataStructures/linkedlist/set.h
@@ -0,0 +1,28 @@
+/*
+ * set.h
+ * C set header file
+ * William Chargin
+ * 4 September 2013
+ */
+#ifndef __SET_H__
+#define __SET_H__
+#include <stddlib.h>
+
+// For semantics
+typedef llist set;
+
+// These three should return true iff $value \in s$ before the method call.
+bool s_add(set *s, int value);
+bool s_remove(set *s, int value);
+bool s_contains(set *s, int value);
+
+// Set operations
+set* s_union(set *s1, set *s2);
+set* s_isect(set *s1, set *s2);
+set* s_diff(set *s1, set *s2); // s1 \ s2; ${x \in s1 | x \notin s2}$
+
+// Axiom of Specification; see ZFC theory
+set* s_spec(set *s, bool (*p) (int));
+
+
+#endif
