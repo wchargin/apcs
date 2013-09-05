d481990c307e7ef1a960dab6e054528ddfb21659
William Chargin <wchargin@gmail.com>
2013-09-05 00:41:12 -0400

    Initial set implementation (untested!)

---
+++ BasicDataStructures/linkedlist/set.c
@@ -0,0 +1,93 @@
+/*
+ * set.c
+ * C set implementation
+ * William Chargin
+ * 4 September 2013
+ */
+#include <stdlib.h>
+#include "set.h"
+
+bool s_add(set *s, int value) {
+    if (!s_contains(s, value)) {
+        ll_push(s, value);
+        return false;
+    } else {
+        return true;
+    }
+}
+
+bool s_remove(set *s, int value) {
+    int index = ll_seek(s, value);
+    if (index != -1) {
+        ll_remove(s, index);
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool s_contains(set *s, int value) {
+    return ll_seek(s, value) != -1;
+}
+
+set* s_union(set *s1, set *s2) {
+    set *dest = malloc(sizeof(set));
+    
+    node *n;
+    n = s1 -> first;
+    while (n != NULL) {
+        s_add(dest, n -> value);
+        n = n -> next;
+    }
+    n = s2 -> first;
+    while (n != NULL) {
+        s_add(dest, n -> value);
+        n = n -> next;
+    }
+    return dest;
+}
+
+set *s_isect(set *s1, set *s2) {
+    set *dest = malloc(sizeof(set));
+    
+    node *n;
+    n = s1 -> first;
+    while (n != NULL) {
+        if (s_contains(s2, value)) {
+            s_add(dest, n -> value);
+        }
+        n = n -> next;
+    }
+    
+    return dest;
+}
+
+set *s_diff(set *s1, set *s2) {
+    set *dest = malloc(sizeof(set));
+    
+    node *n;
+    n = s1 -> first;
+    while (n != NULL) {
+        if (!s_contains(s2, value)) {
+            s_add(dest, n -> value);
+        }
+        n = n -> next;
+    }
+    
+    return dest;
+}
+
+set* s_spec(set *s, bool (*p) (int)) {
+    set *dest = malloc(sizeof(set));
+    
+    node *n;
+    n = s1 -> first;
+    while (n != NULL) {
+        if ((*p)(n -> value)) {
+            s_add(dest, n -> value);
+        }
+        n = n -> next;
+    }
+    
+    return dest;
+}
