4bf137969a58c484d4c13be209756f70a3d6b03f
William Chargin <wchargin@gmail.com>
2013-09-07 12:10:45 -0400

    Binary tree specification (header file) begun

---
+++ BasicDataStructures/bintree/bintree.h
@@ -0,0 +1,34 @@
+/*
+ * bintree.h
+ * C binary search tree header file
+ * William Chargin
+ * 7 September 2013
+ */
+#ifndef __BINTREE_H__
+#define __BINTREE_H__
+
+#include <stdbool.h>
+
+typedef struct node {
+    int key;
+    int value;
+    struct node *parent;
+    struct node *left;
+    struct node *right;
+} node;
+
+typedef struct bintree {
+    node *root;
+    int size;
+} bintree;
+
+// Create a new binary tree
+bintree* bt_new();
+
+bintree* bt_add(bintree *tree, int key, int value);
+
+void bt_optimize(bintree* tree);
+
+void bt_traverse(bintree *tree, void (*callback) (node*));
+
+#endif
