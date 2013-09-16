c5a2bc63a355f01d9aea03ad0cf4de9e683db1e1
William Chargin <wchargin@gmail.com>
2013-09-15 20:13:27 -0400

    Singly linked list declaration

---
+++ BasicDataStructures/v2/llist.h
@@ -0,0 +1,49 @@
+/*
+ * llist.h
+ * C singly-linked list header file
+ * William Chargin
+ * 15 September 2013
+ */
+
+#ifndef __LLIST_H__
+#define __LLIST_H__
+
+#include <stdbool.h>
+
+typedef struct node {
+    int value;
+    struct node* next;
+} node;
+
+typedef struct llist {
+    node *head;
+} llist;
+
+/* create new list properly */
+llist* ll_new();
+
+/* add to front */
+void ll_addf(llist* l, int value);
+
+/* add to back */
+void ll_addb(llist* l, int value);
+
+/* remove from front */
+void ll_popf(llist *l);
+
+/* remove from back */
+void ll_popb(llist *l);
+
+/* get index of the first occurrence */
+int ll_find(llist *l, int value);
+
+/* remove index */
+void ll_rmat(llist *l, int index);
+
+/* remove first occurrence of value */
+void ll_rmval(llist *l, int value);
+
+/* tests whether the list is cyclic */
+bool ll_cycle(llist *l);
+
+#endif
