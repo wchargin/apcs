032acbc790279f6de4b6a9e52787d82aea84113e
William Chargin <wchargin@gmail.com>
2013-09-16 11:53:36 -0400

    Parentless binary tree header file

---
+++ BasicDataStructures/v2/bintree.h
@@ -0,0 +1,34 @@
+/*
+ * bintree.h
+ * C parentless binary tree header file
+ * William Chargin
+ * 16 September 2013
+ */
+#ifndef __BINTREE_H__
+#define __BINTREE_H__
+
+#include <stdbool.h>
+
+typedef struct btnode {
+    int value;
+    struct btnode* left;
+    struct btnode* right;
+} btnode;
+
+typedef struct bintree {
+    btnode* root;
+} bintree;
+
+/* create and initialize new tree */
+bintree* bt_new();
+
+/* add to tree */
+void bt_add(bintree *tree, int value);
+
+/* remove from tree */
+void bt_remove(bintree *tree, int value);
+
+/* test if value is in tree */
+bool bt_contains(bintree *tree, int value);
+
+#endif
