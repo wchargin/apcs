6f137bbddc40fb9881d90d5419f7e6cae18f0003
William Chargin <wchargin@gmail.com>
2013-09-28 08:19:00 -0700

    Add a bit to Kruskal test

--- Graphs/src/graphs/tests/KruskalTest.java
+++ Graphs/src/graphs/tests/KruskalTest.java
@@ -1,11 +1,18 @@
 package graphs.tests;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
+import graphs.core.AdjacencyGraph;
+import graphs.core.EdgeGenerator;
+import graphs.core.Node;
+import graphs.core.WeightedEdge;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class KruskalTest {
+	
+	private AdjacencyGraph<Integer, Node<Integer>, WeightedEdge<Integer, Double>> graph;
+	private EdgeGenerator<Integer, WeightedEdge<Integer, Double>> gen;
 
 	@Before
 	public void setUp() throws Exception {
