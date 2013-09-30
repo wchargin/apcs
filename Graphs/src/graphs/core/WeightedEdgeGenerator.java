5f3349017fc9f689396947c85d7a125904280362
wchargin <wchargin@gmail.com>
2013-09-30 08:07:34 -0700

    Weighted edge generator (ugh)

---
+++ Graphs/src/graphs/core/WeightedEdgeGenerator.java
@@ -0,0 +1,33 @@
+package graphs.core;
+
+/**
+ * A functional interface for generating a specific type of edge for given
+ * values at runtime.
+ * 
+ * @author William Chargin
+ * 
+ * @param <T>
+ *            the type of values represented by the nodes of the edge
+ */
+public abstract class WeightedEdgeGenerator<T, W extends Comparable<W>, E extends WeightedEdge<T, W>> {
+
+	/**
+	 * The next weight to be used by this weight generator.
+	 */
+	public W nextWeight;
+	
+	/**
+	 * Generates an edge {@code e} that "connects" the two given nodes,
+	 * according to the edge's definition of
+	 * {@linkplain Edge#connects(Node, Node) connection}.
+	 * 
+	 * @param n1
+	 *            the first node
+	 * @param n2
+	 *            the second node
+	 * @return some edge {@code e} such that {@link Edge#connects(Node, Node)
+	 *         e.connects(n1, n2)} is {@code true}.
+	 */
+	public abstract E createEdge(Node<T> n1, Node<T> n2);
+
+}
