00a504139966d12c66d17d0376a0ca77355e2b4e
wchargin <wchargin@gmail.com>
2013-10-03 13:06:53 -0700

    Begin Graphs GUI

---
+++ Graphs/src/graphs/gui/GraphsGUI.java
@@ -0,0 +1,32 @@
+package graphs.gui;
+
+import java.awt.Color;
+
+import jgame.GRootContainer;
+import jgame.Game;
+
+public class GraphsGUI extends Game {
+
+	public GraphsGUI() {
+		GRootContainer root = new GRootContainer(Color.WHITE);
+		setRootContainer(root);
+
+		GraphSettings gs = new GraphSettings();
+
+		root.addView(Views.GRAPHS, new GraphsView(gs));
+		root.addView(Views.SETTINGS, new SettingsView(gs));
+	}
+
+	public enum Views {
+		GRAPHS, SETTINGS;
+	}
+
+	public class GraphSettings {
+		public float textSize = 12;
+	}
+
+	public static void main(String[] args) {
+		new GraphsGUI().startGame("Graphs");
+	}
+
+}
---
+++ Graphs/src/graphs/gui/GraphsView.java
@@ -0,0 +1,29 @@
+package graphs.gui;
+
+import graphs.gui.GraphsGUI.GraphSettings;
+import graphs.gui.GraphsGUI.Views;
+import jgame.Context;
+import jgame.GButton;
+import jgame.GContainer;
+import jgame.GMessage;
+import jgame.listener.ButtonListener;
+
+public class GraphsView extends GContainer {
+
+	public GraphsView(GraphSettings gs) {
+		setSize(800, 600);
+		
+		GMessage msgSettings = new GMessage("Settings");
+		GButton btnSettings = new GButton();
+		btnSettings.setSize(100, 30);
+		btnSettings.addAtCenter(msgSettings);
+		addAtCenter(btnSettings);
+		btnSettings.addListener(new ButtonListener() {
+			@Override
+			public void mouseClicked(Context context) {
+				context.setCurrentGameView(Views.SETTINGS);
+			}
+		});
+	}
+
+}
