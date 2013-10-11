ca660a713189bccbf0a1fac1d5f0c91b78ceaea6
wchargin <wchargin@gmail.com>
2013-10-11 16:19:51 -0700

    Add cookies

--- jDisjointSets/src/disjoint/maze/jgamegui/MazeGUI.java
+++ jDisjointSets/src/disjoint/maze/jgamegui/MazeGUI.java
@@ -2,8 +2,11 @@ package disjoint.maze.jgamegui;
 
 import java.awt.Color;
 
+import jgame.GContainer;
 import jgame.GRootContainer;
+import jgame.GSprite;
 import jgame.Game;
+import jgame.ImageCache;
 
 public class MazeGUI extends Game {
 
@@ -13,15 +16,20 @@ public class MazeGUI extends Game {
 
 	public MazeGUI() {
 		GRootContainer root = new GRootContainer(Color.WHITE);
-		
+
 		MazeView mv = new MazeView();
 		root.addView(Views.MAZE_VIEW, mv);
-		
+
+		ImageCache.create(MazeGUI.class, "/disjoint/maze/jgamegui/");
+		root.addView(Views.WIN,
+				new GContainer(new GSprite(ImageCache.forClass(MazeGUI.class)
+						.get("cookie.png"))));
+
 		setRootContainer(root);
 	}
-	
+
 	public enum Views {
-		MAZE_VIEW;
+		MAZE_VIEW, WIN;
 	}
 
 }
