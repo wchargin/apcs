23e1e55e49904fbf15653d442112315908424deb
William Chargin <wchargin@gmail.com>
2013-10-11 17:12:56 -0700

    Winning improvements

--- jDisjointSets/src/disjoint/maze/jgamegui/MazeGUI.java
+++ jDisjointSets/src/disjoint/maze/jgamegui/MazeGUI.java
@@ -2,29 +2,23 @@ package disjoint.maze.jgamegui;
 
 import java.awt.Color;
 
-import jgame.GContainer;
 import jgame.GRootContainer;
-import jgame.GSprite;
 import jgame.Game;
 import jgame.ImageCache;
 
 public class MazeGUI extends Game {
 
 	public static void main(String[] args) {
+		ImageCache.create(MazeGUI.class, "/disjoint/maze/jgamegui/");
 		new MazeGUI().startGame();
 	}
 
 	public MazeGUI() {
 		GRootContainer root = new GRootContainer(Color.WHITE);
 
-		MazeView mv = new MazeView();
-		root.addView(Views.MAZE_VIEW, mv);
-
-		ImageCache.create(MazeGUI.class, "/disjoint/maze/jgamegui/");
-		root.addView(Views.WIN,
-				new GContainer(new GSprite(ImageCache.forClass(MazeGUI.class)
-						.get("cookie.png"))));
-
+		root.addView(Views.MAZE_VIEW, new MazeView());
+		root.addView(Views.WIN, new WinView());
+		
 		setRootContainer(root);
 	}
 
