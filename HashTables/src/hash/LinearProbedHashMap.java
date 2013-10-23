34d8dc429d22a114ad72486657098e211a7d1542
wchargin <wchargin@gmail.com>
2013-10-23 08:29:21 -0700

    Create a generic ProbedHashMap class
    also modify LinearProbedHashMap to extend this

--- HashTables/src/hash/LinearProbedHashMap.java
+++ HashTables/src/hash/LinearProbedHashMap.java
@@ -1,11 +1,5 @@
 package hash;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Map.Entry;
-import java.util.Set;
-
 /**
  * A hash map that uses linear probing to resolve collisions.
  * 
@@ -16,18 +10,7 @@ import java.util.Set;
  * @param <V>
  *            the type of values stored in this map
  */
-public class LinearProbedHashMap<K, V> extends ArrayHashMap<K, V, Entry<K, V>> {
-
-	/**
-	 * The size of this array (number of key-value pairs), maintained for easy
-	 * computation.
-	 */
-	private int size;
-
-	/**
-	 * The entry used to indicate a null value.
-	 */
-	private final Entry<K, V> empty = new SimpleEntry<K, V>(null, null);
+public class LinearProbedHashMap<K, V> extends ProbedHashMap<K, V> {
 
 	/**
 	 * Creates the map with the given hash function and default load factor.
@@ -52,149 +35,8 @@ public class LinearProbedHashMap<K, V> extends ArrayHashMap<K, V, Entry<K, V>> {
 	}
 
 	@Override
-	protected int calculatePosition(K key) {
-		while (true) {
-			int calculated = calculatePositionPassive(key);
-			if (calculated >= 0) {
-				// valid position found
-				return calculated;
-			} // else
-				// no position found
-				// remap and try again
-			remapDouble();
-		}
-	}
-
-	/**
-	 * Calculates the array position for a given key. Unlike
-	 * {@link #calculatePosition(Object)}, this will <em>not</em> force a remap
-	 * if the key has no valid position; instead, it will simply return
-	 * {@code -1}.
-	 * 
-	 * @param key
-	 *            the key whose position to find
-	 * @return the position, or {@code -1} if the key is not in the map and
-	 *         there is no space
-	 */
-	private int calculatePositionPassive(K key) {
-		final int originalPos = super.calculatePosition(key);
-		int pos = originalPos;
-
-		// check the position for validity at least once, and until we find
-		// it or loop back to the beginning
-		//
-		// validity: slot is null or contains the same key
-		do {
-			if (list[pos] == null || list[pos] == empty
-					|| equals(list[pos].getKey(), key)) {
-				// this is the correct position
-				return pos;
-			}
-			pos = (pos + 1) % list.length;
-		} while (originalPos != pos /* haven't looped */);
-
-		// we looped around; no valid position
-		// fail
-		return -1;
-	}
-
-	@Override
-	public void clear() {
-		Arrays.fill(list, null);
-		size = 0;
-	}
-
-	@Override
-	public boolean containsKey(K key) {
-		for (Entry<K, V> entry : list) {
-			if (isValidEntry(entry) && equals(entry.getKey(), key)) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	@Override
-	public boolean containsValue(V value) {
-		for (Entry<K, V> entry : list) {
-			if (isValidEntry(entry) && equals(entry.getValue(), value)) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	@Override
-	public Set<? extends Entry<K, V>> entrySet() {
-		HashSet<Entry<K, V>> entries = new HashSet<>();
-		for (Entry<K, V> entry : list) {
-			if (isValidEntry(entry)) {
-				// don't count the empty entry
-				// this also fixes a potential problem in #remap(int)
-				entries.add(entry);
-			}
-		}
-		return entries;
-	}
-
-	@Override
-	public V get(K key) {
-		final Entry<K, V> at = list[calculatePositionPassive(key)];
-		return at == null ? null : at.getValue();
-	}
-
-	@SuppressWarnings("unchecked")
-	@Override
-	protected void initializeList(int size) {
-		list = (SimpleEntry[]) Array.newInstance(SimpleEntry.class, size);
-		clear();
-	}
-
-	/**
-	 * Tests if the current entry is non-{@code null} and non-{@link #empty}.
-	 * 
-	 * @param entry
-	 *            the entry to test
-	 * @return {@code true} if it is valid, or {@code false} if it is
-	 *         {@code null} or {@link #empty}
-	 */
-	private boolean isValidEntry(Entry<K, V> entry) {
-		return entry != null && entry != empty;
-	}
-
-	@Override
-	public V put(K key, V value) {
-		// the calculatePosition method will force remap until we have validity
-		int pos = calculatePosition(key);
-		Entry<K, V> entry = list[pos];
-		if (entry != null) {
-			return entry.setValue(value);
-		} else {
-			list[pos] = new SimpleEntry<K, V>(key, value);
-			size++;
-			remapIfNecessary();
-			return null;
-		}
-	}
-
-	@Override
-	public V remove(K key) {
-		int pos = calculatePositionPassive(key);
-		final Entry<K, V> at = list[pos];
-		if (at == null || at == empty) {
-			// not in map
-			return null;
-		} else {
-			V value = at.getValue();
-			list[pos] = empty;
-			size--;
-			return value;
-		}
-	}
-
-	@Override
-	public int size() {
-		return size;
+	protected int getProbeValue(int i) {
+		return i;
 	}
 
 }
