e91dde7585ca2e3a768a0e19f9f23756034ca15b
William Chargin <wchargin@gmail.com>
2013-11-20 16:51:08 -0800

    Simplify producer with SimpleFileVisitor

--- ThreadSync/src/prodcons/FileTreeProducer.java
+++ ThreadSync/src/prodcons/FileTreeProducer.java
@@ -2,9 +2,9 @@ package prodcons;
 
 import java.io.IOException;
 import java.nio.file.FileVisitResult;
-import java.nio.file.FileVisitor;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
@@ -32,19 +32,7 @@ public class FileTreeProducer implements Producer<Path> {
 	 */
 	public FileTreeProducer(Path root) {
 		try {
-			Files.walkFileTree(root, new FileVisitor<Path>() {
-				@Override
-				public FileVisitResult postVisitDirectory(Path dir,
-						IOException exc) throws IOException {
-					return FileVisitResult.CONTINUE;
-				}
-
-				@Override
-				public FileVisitResult preVisitDirectory(Path dir,
-						BasicFileAttributes attrs) throws IOException {
-					return FileVisitResult.CONTINUE;
-				}
-
+			Files.walkFileTree(root, new SimpleFileVisitor<Path>() {
 				@Override
 				public FileVisitResult visitFile(Path file,
 						BasicFileAttributes attrs) throws IOException {
@@ -53,13 +41,7 @@ public class FileTreeProducer implements Producer<Path> {
 					} catch (InterruptedException e) {
 						e.printStackTrace();
 					}
-					return FileVisitResult.CONTINUE;
-				}
-
-				@Override
-				public FileVisitResult visitFileFailed(Path file,
-						IOException exc) throws IOException {
-					return FileVisitResult.CONTINUE;
+					return super.visitFile(file, attrs);
 				}
 			});
 		} catch (IOException e) {
