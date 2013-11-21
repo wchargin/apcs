8a41db15cc34dd6474b84cf87b8cf57f6fb956e7
William Chargin <wchargin@gmail.com>
2013-11-20 16:57:37 -0800

    Modify test driver to use indexer

--- ThreadSync/src/prodcons/FileTreeApplication.java
+++ ThreadSync/src/prodcons/FileTreeApplication.java
@@ -2,14 +2,26 @@ package prodcons;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.Scanner;
 
 public class FileTreeApplication {
 
 	public static void main(String[] args) {
-		ApplicationServer<Path> s = new ApplicationServer<>(100);
-		s.registerConsumer(new FileReadConsumer());
-		s.registerProducer(new FileTreeProducer(Paths
-				.get("H:\\git\\blender")));
+		try (Scanner sc = new Scanner(System.in)) {
+			ApplicationServer<Path> s = new ApplicationServer<>(1000);
+			final Index<String, Path> index = new Index<>();
+			s.registerConsumer(new FileIndexingConsumer(index));
+			System.out.print("Enter root path: ");
+			s.registerProducer(new FileTreeProducer(Paths.get(sc.nextLine())));
+			while (true) {
+				System.out.print("Enter search string: ");
+				String in = sc.nextLine();
+				final List<Path> result = index.get(in.toLowerCase());
+				System.out.println(result == null ? "0" : (result.size()
+						+ " : " + result));
+			}
+		}
 	}
 
 }
