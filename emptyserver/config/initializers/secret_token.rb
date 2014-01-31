6b2b2917555d0d680732efc2c83422ab465896ca
WChargin <wchargin@gmail.com>
2014-01-30 20:24:40 -0800

    Create emptyserver as a Rails project

---
+++ emptyserver/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Emptyserver::Application.config.secret_key_base = 'dcdfd1868e5ae406be1ad8cfeefede972d8ae424c9141758975e10cdd171d423cb24c5e657bed35ea19279ce948ba8b8d99d9ffc770b39e2a16b768f79d5ede0'
---
+++ emptyserver/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Emptyserver::Application.config.session_store :cookie_store, key: '_emptyserver_session'
