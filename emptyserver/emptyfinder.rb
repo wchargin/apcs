d08953639260334b207aef86b933ebd244ad80bc
William Chargin <wchargin@gmail.com>
2014-02-01 23:35:46 -0500

    Switch find_empties from Set to Array and remove prefix strip

--- emptyserver/emptyfinder.rb
+++ emptyserver/emptyfinder.rb
@@ -1,14 +1,11 @@
-require 'set'
-
-def find_empties directory, dirprefix, fsuffix
-  empty_dirs = Set.new
+def find_empties(directory, dirprefix, fsuffix)
+  empty_dirs = Array.new
   Dir.chdir directory do
     Dir["#{dirprefix}**/"].each do |subdir|
       Dir.chdir subdir do
-        dirsuffix = subdir[1 .. -dirprefix.length]
-        empty_dirs.add dirsuffix if Dir["**.#{fsuffix}"].empty?
+        empty_dirs.push subdir if Dir["**.#{fsuffix}"].empty?
       end
     end
   end
-  return empty_dirs.collect
+  return empty_dirs
 end
