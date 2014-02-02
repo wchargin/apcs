5f4754eddf89058324e634ef467494dadde0582e
William Chargin <wchargin@gmail.com>
2014-02-01 23:37:34 -0500

    Implement sorter by numeric that falls back to lexicographic

--- emptyserver/emptyfinder.rb
+++ emptyserver/emptyfinder.rb
@@ -9,3 +9,29 @@ def find_empties(directory, dirprefix, fsuffix)
   end
   return empty_dirs
 end
+
+def find_empties_sorted(directory, dirprefix, fsuffix)
+  empties = find_empties(directory, dirprefix, fsuffix)
+  nums = Hash.new
+  non_numeric = Array.new
+  empties.each do |x|
+    # if starts with digits, trim leading zeroes and extract; else, nothing
+    num = x.sub(/^#{Regexp.escape dirprefix}0*(\d+).*/, '\1')
+    is_num = num !~ /\D/
+    if is_num
+      n = Integer num
+      nums[n] = Array.new unless nums[n]
+      nums[n].push x
+    else
+      non_numeric.push x
+    end
+  end
+  result = Array.new
+  nums.sort.each do |k, v|
+    v.each do |x|
+      result.push x
+    end
+  end
+  result.concat non_numeric.sort
+  result
+end
