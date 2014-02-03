7ce81cca834df19f461e5056cc2372538194fbd5
William Chargin <wchargin@gmail.com>
2014-02-03 11:11:06 -0500

    Implement suggestions from CodeReview.SE
    at http://codereview.stackexchange.com/q/40655/29124

--- emptyserver/emptyfinder.rb
+++ emptyserver/emptyfinder.rb
@@ -1,5 +1,5 @@
 def find_empties(directory, dirprefix, fsuffix)
-  empty_dirs = Array.new
+  empty_dirs = []
   Dir.chdir directory do
     Dir["#{dirprefix}**/"].each do |subdir|
       Dir.chdir subdir do
@@ -7,31 +7,23 @@ def find_empties(directory, dirprefix, fsuffix)
       end
     end
   end
-  return empty_dirs
+  empty_dirs
 end
 
 def find_empties_sorted(directory, dirprefix, fsuffix)
   empties = find_empties(directory, dirprefix, fsuffix)
-  nums = Hash.new
-  non_numeric = Array.new
+  nums = Hash.new { |h,k| h[k] = [] }
+  non_numeric = []
   empties.each do |x|
     # if starts with digits, trim leading zeroes and extract; else, nothing
     num = x.sub(/^#{Regexp.escape dirprefix}0*(\d+).*/, '\1')
-    is_num = num !~ /\D/
-    if is_num
-      n = Integer num
-      nums[n] = Array.new unless nums[n]
-      nums[n].push x
+    if num !~ /\D/ # is a number
+      nums[Integer num].push x
     else
       non_numeric.push x
     end
   end
-  result = Array.new
-  nums.sort.each do |k, v|
-    v.each do |x|
-      result.push x
-    end
-  end
+  result = []
+  nums.sort.each { |k, v| result.concat v }
   result.concat non_numeric.sort
-  result
 end
