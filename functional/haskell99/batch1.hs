fd7c016b9d0d4c31134981e3f968f29b9170ef48
WChargin <wchargin@gmail.com>
2014-05-13 15:52:51 -0700

    Complete problem 06

--- functional/haskell99/batch1.hs
+++ functional/haskell99/batch1.hs
@@ -23,3 +23,6 @@ length' (x:xs) = 1 + length' xs
 reverse' :: [a] -> [a]
 reverse' [] = []
 reverse' (x:xs) = reverse' xs ++ [x]
+
+isPalindrome :: (Eq a) => [a] -> Bool
+isPalindrome xs = xs == reverse xs
