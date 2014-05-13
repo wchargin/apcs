b452e2a99684aa0342ffc5fc0ec63802200148f5
William Chargin <wchargin@gmail.com>
2014-05-13 15:38:29 +0000

    Complete problem 02

--- functional/haskell99/batch1.hs
+++ functional/haskell99/batch1.hs
@@ -4,3 +4,9 @@ lastElem :: [a] -> a
 lastElem [] = error "empty list"
 lastElem [x] = x
 lastElem (x:xs) = lastElem xs
+
+secondLastElem :: [a] -> a
+secondLastElem [] = error "empty list"
+secondLastElem [x] = error "list too short"
+secondLastElem [x,y] = x
+secondLastElem (x:xs) = lastElem xs
