d0b6465a15eb41622d096a1855d541a92d061a31
WChargin <wchargin@gmail.com>
2014-05-13 17:43:22 -0700

    Complete problem 11

--- functional/haskell99/batch2.hs
+++ functional/haskell99/batch2.hs
@@ -1,1 +1,10 @@
 module Batch2 where
+
+import Data.List
+data MultiElement a = Single a | Multiple Int a deriving (Show)
+
+encodeModified :: (Eq a) => [a] -> [MultiElement a]
+encodeModified = map f . group
+  where f xs = (case length xs of 1 -> Single
+                                  _ -> Multiple (length xs))
+               (head xs)
