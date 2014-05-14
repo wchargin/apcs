7c14e1d5a7ecad16f4344f098d5c346217a04a3a
William Chargin <wchargin@gmail.com>
2014-05-14 15:15:14 +0000

    Complete problem 22

--- functional/haskell99/batch3.hs
+++ functional/haskell99/batch3.hs
@@ -1,5 +1,21 @@
 module Batch3 where
 
+import Data.Functor
+import System.Random
+
 insertAt :: Int -> a -> [a] -> [a]
 insertAt 0 x ys = x:ys
 insertAt n x l@(y:ys) = y : insertAt (n - 1) x ys
+
+-- Again, using half-open intervals [a, b).
+range :: Int -> Int -> [Int]
+range x y = case x >= y of
+  True  -> []
+  False -> x:range (x + 1) y
+
+randomSelect :: [a] -> Int -> IO [a]
+randomSelect _ 0 = return []
+randomSelect xs n = do
+  index <- randomRIO (0, length xs - 1)
+  let elem = xs !! index
+  (elem:) <$> randomSelect xs (n - 1)
