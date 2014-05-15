6b2437ce6538849fd923a8c97a0bb7f59e4b6f85
William Chargin <wchargin@gmail.com>
2014-05-15 15:27:31 +0000

    Complete problem 32

--- functional/haskell99/batch4.hs
+++ functional/haskell99/batch4.hs
@@ -3,3 +3,8 @@ module Batch4 where
 isPrime :: Int -> Bool
 isPrime x = and [x `mod` n /= 0 | n <- [2..max]]
   where max = ceiling $ sqrt $ fromIntegral x
+
+gcd' :: Int -> Int -> Int
+gcd' a b
+  | b == 0    = abs a
+  | otherwise = gcd' b $ a `mod` b
